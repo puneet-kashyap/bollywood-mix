a17af73678f42855aacaf09a21f4521c9ec0ca8f
Puneet Kashyap <pkashyaps@gmail.com>
2018-02-21 10:20:37 -0500

    two more youtube videos added

--- src/Components/Archives.js
+++ src/Components/Archives.js
@@ -6,6 +6,8 @@ import AppHeader from './AppHeader';
 import AppFooter from './AppFooter';
 
 const youtubeVideos = [
+  "https://www.youtube.com/embed/MREsQgfDpdU",
+  "https://www.youtube.com/embed/oRm6pJu0f7M",
   "https://www.youtube.com/embed/J5VPQ2dWxIU",
   "https://www.youtube.com/embed/BL6CTT4h9zw"
 
@@ -19,7 +21,7 @@ const Archives  = () => {
         <Typography type="display1" component="h1" color="primary" style={{'padding':'25px','text-align':'center'}}>
           Bollywood Mix Archives
         </Typography>
-      <div className="row" style={{'justifyContent':'center'}}>
+      <div className="row">
           {youtubeVideos.map(item =>
             <div key={item}  id={item} className="col-md-4 text-center" style={{'padding':'15px'}}>
               <Card raised>
