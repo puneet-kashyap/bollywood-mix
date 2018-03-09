139d9babfcf4abf4c3d69380f9849389087aa9bb
Puneet Kashyap <pkashyaps@gmail.com>
2018-03-09 15:08:06 -0500

    notify request button added

--- src/Components/ShowTimings.js
+++ src/Components/ShowTimings.js
@@ -2,10 +2,6 @@ import React from 'react';
 import Card, { CardContent } from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
 import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
-import PlayArrowIcon from 'material-ui-icons/PlayArrow';
-import Tooltip from 'material-ui/Tooltip';
-import Button from 'material-ui/Button';
-import {requestPermission} from '../fire';
 import Advertisement from './Utils/Advertisement';
 
 export const ShowTimings = () => {
@@ -22,95 +18,56 @@ export const ShowTimings = () => {
 
 return(
   <div className="container">
-  <div className="row" style={{'padding':'15px','justifyContent':'center','alignItems':'center'}}>
+    <div className="row" style={{'padding':'15px','justifyContent':'center','alignItems':'center'}}>
 
-    <div className="col-md-3">
-      <Advertisement id="first-time-buyers-ad" src={require('../Images/Andy_ad1.jpeg')}
-        href="https://andynagpal.com/first-time-buyers"/>
-    </div>
+      <div className="col-md-3">
+        <Advertisement id="first-time-buyers-ad" src={require('../Images/Andy_ad1.jpeg')}
+          href="https://andynagpal.com/first-time-buyers"/>
+      </div>
 
-      <div className="col-md-6">
-          <Card style={{
-            'textAlign':'center',
-            'margin':'10px',
-            'padding':'5px',
-            'background':'ghostwhite'
-          }}>
-              <div className="align-self-center">
-                <CardContent>
-                  <Typography type="display1" color="primary">Show Timings</Typography>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>Day</TableCell>
-                        <TableCell>Timings</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {weekdays.map(item =>
-                          <TableRow key={item.day}>
-                              <TableCell padding='none'>{item.day}</TableCell>
-                              <TableCell padding='none'>{item.time}</TableCell>
-                          </TableRow>
-                      )}
-                    </TableBody>
-                  </Table>
-                </CardContent>
-              </div>
-          </Card>
-    </div>
+        <div className="col-md-6">
+            <Card style={{
+              'textAlign':'center',
+              'margin':'10px',
+              'padding':'5px',
+              'background':'ghostwhite'
+            }}>
+                <div className="align-self-center">
+                  <CardContent>
+                    <Typography type="display1" color="primary">Show Timings</Typography>
+                    <Table>
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Day</TableCell>
+                          <TableCell>Timings</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {weekdays.map(item =>
+                            <TableRow key={item.day}>
+                                <TableCell padding='none'>{item.day}</TableCell>
+                                <TableCell padding='none'>{item.time}</TableCell>
+                            </TableRow>
+                        )}
+                      </TableBody>
+                    </Table>
+                  </CardContent>
+                </div>
+            </Card>
+      </div>
 
-    <div className="col-md-3 d-none d-md-block">
-      <Advertisement  id="free-home-evaluation-ad" src={require('../Images/Home_Evaluation_ad.png')}
-        href="https://andynagpal.com/free-home-evaluation"/>
-    </div>
-
-  </div>
-  <div className="row d-sm-none" style={{'padding':'15px','justifyContent':'center'}}>
-      <div className="col-md-4">
+      <div className="col-md-3 d-none d-md-block">
         <Advertisement  id="free-home-evaluation-ad" src={require('../Images/Home_Evaluation_ad.png')}
           href="https://andynagpal.com/free-home-evaluation"/>
       </div>
-  </div>
+
+    </div>
+    <div className="row d-sm-none" style={{'padding':'15px','justifyContent':'center'}}>
+        <div className="col-md-4">
+          <Advertisement  id="free-home-evaluation-ad" src={require('../Images/Home_Evaluation_ad.png')}
+            href="https://andynagpal.com/free-home-evaluation"/>
+        </div>
+    </div>
   </div>
 )
-}
-
-export const NextShow = () => {
-  return(
-    <div className="container col-md-6" style={{'padding':'15px'}}>
-      <Card style={{
-        'textAlign':'center',
-        'margin':'10px',
-        'padding':'5px',
-        'background':'ghostwhite'
-      }}>
-      <div className="align-self-center" style={{'paddingBottom':'16px'}}>
-        <CardContent>
-          <Typography type="display1" color="primary">Next Show</Typography>
-          Bollywood Mix will be live soon at 10:00 am EST.
-        </CardContent>
-
-        <Tooltip title="BollywoodMix Show in not live right now.">
-        <div id="disabled-play-button" style={{'alignItems':'center'}}>
-              <Button raised aria-label="Play/pause" color="secondary" disabled>
-                  Play
-                  <PlayArrowIcon style={{'marginLeft':'5px'}}/>
-              </Button>
-        </div>
-        </Tooltip>
-
-        <Tooltip title="We will notify you once the show is Live.">
-        <div id="notify-me" style={{'alignItems':'center', 'paddingTop':'25px'}}>
-              <Button raised aria-label="Notify Me" color="primary" onClick={requestPermission}>
-                  Notify Me
-              </Button>
-        </div>
-        </Tooltip>
-
-      </div>
-    </Card>
-
-  </div>
-  )
 }
