b05dcb910b2406a330a0327b19e4559a96c5a866
Puneet Kashyap <pkashyaps@gmail.com>
2018-03-21 12:32:32 -0400

    dynamically added next show time

--- src/Components/Utils/TimeMoment.js
+++ src/Components/Utils/TimeMoment.js
@@ -2,21 +2,23 @@ import moment from 'moment-timezone';
 import 'moment-duration-format';
 
 export const countDownShowTime = () => {
-  var estTime = moment().tz("America/Toronto");
-  var showTime = moment().tz("America/Toronto").set({'hour':10,'minute':0,'second':0});
-  var eveningShow = moment().tz("America/Toronto").set({'hour':17,'minute':0,'second':0});
+  const estTime = moment().tz("America/Toronto");
+  const showTime = moment().tz("America/Toronto").set({'hour':10,'minute':0,'second':0});
+  const eveningShow = moment().tz("America/Toronto").set({'hour':17,'minute':0,'second':0});
 
-  var nextDay = moment.duration(moment().tz("America/Toronto").set(
+  const nextDay = moment.duration(moment().tz("America/Toronto").set(
     {'hour':10,'minute':0,'second':0}).add(1,'day').diff(estTime)).as('minutes');
-  var dayAfterTomorrow = moment.duration(moment().tz("America/Toronto").set(
+  const dayAfterTomorrow = moment.duration(moment().tz("America/Toronto").set(
     {'hour':10,'minute':0,'second':0}).add(2,'day').diff(estTime)).as('minutes');
 
   var duration, remainingTime;
+  let nextShowTime = showTime;
 
   if (estTime.day() > 0 && estTime.day() < 6) {
     // Monday to Friday
     if (estTime.day() === 5 && estTime.hours() > 11 && estTime.hours() < 17) {
       //Frday between 10 and 5
+      nextShowTime = eveningShow;
       duration = moment.duration(eveningShow.diff(estTime)).asMinutes();
     } else if(estTime.hours() < 10) {
       // monday to friday before 10 am
@@ -34,5 +36,5 @@ export const countDownShowTime = () => {
     duration = nextDay;
   }
   remainingTime = moment.duration(duration, "minutes").format("hh:mm:ss");
-  return remainingTime;
+  return {remainingTime: remainingTime, nextShowTime: nextShowTime};
 }
