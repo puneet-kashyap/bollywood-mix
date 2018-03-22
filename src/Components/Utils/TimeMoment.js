e4d2e2afe4b0bb016ab4ffb118223f0c3bc6cc0b
Puneet Kashyap <pkashyaps@gmail.com>
2018-03-22 11:42:16 -0400

    code refactored to use factory function instead of constructors

--- src/Components/Utils/TimeMoment.js
+++ src/Components/Utils/TimeMoment.js
@@ -7,36 +7,32 @@ export const LocalTime = (estTime) => {
   // local time zone of the user
   const localZone = moment.tz.guess();
   if (estTime) {
-    return new EstTime().set({'hour':estTime,'minute':0,'second':0}).tz(localZone);
+    return EstTime().set({'hour':estTime,'minute':0,'second':0}).tz(localZone);
   } else {
-    return new EstTime().tz(localZone);
+    return EstTime().tz(localZone);
   }
 }
 
 export const countDownShowTime = () => {
-  const estTime = new EstTime();
-  const showTime = new EstTime().set({'hour':10,'minute':0,'second':0});
-  const eveningShow = new EstTime().set({'hour':17,'minute':0,'second':0});
+  const estTime = EstTime();
+  const showTime = () => EstTime().set({'hour':10,'minute':0,'second':0});
+  const eveningShow = () => EstTime().set({'hour':17,'minute':0,'second':0});
 
-  const nextDay = moment.duration(
-    new EstTime().set({'hour':10,'minute':0,'second':0}).add(1,'day').diff(estTime)
-  ).as('minutes');
-  const dayAfterTomorrow = moment.duration(
-    new EstTime().set({'hour':10,'minute':0,'second':0}).add(2,'day').diff(estTime)
-  ).as('minutes');
+  const nextDay = moment.duration(showTime().add(1,'day').diff(estTime)).as('minutes');
+  const dayAfterTomorrow = moment.duration(showTime().add(2,'day').diff(estTime)).as('minutes');
 
   var duration, remainingTime;
-  let nextShowTime = showTime;
+  let nextShowTime = showTime();
 
   if (estTime.day() > 0 && estTime.day() < 6) {
     // Monday to Friday
     if (estTime.day() === 5 && estTime.hours() > 11 && estTime.hours() < 17) {
       //Frday between 10 and 5
-      nextShowTime = eveningShow;
-      duration = moment.duration(eveningShow.diff(estTime)).asMinutes();
+      nextShowTime = eveningShow();
+      duration = moment.duration(eveningShow().diff(estTime)).asMinutes();
     } else if(estTime.hours() < 10) {
       // monday to friday before 10 am
-      duration = moment.duration(showTime.diff(estTime)).asMinutes();
+      duration = moment.duration(showTime().diff(estTime)).asMinutes();
     }
     else {
       // monday to friday after 10 am
