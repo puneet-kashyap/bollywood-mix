2f8207816d20006097f1d848a0426f4c94b66235
Puneet Kashyap <pkashyaps@gmail.com>
2018-03-23 14:29:39 -0400

    song request form connected to database

--- src/fire.js
+++ src/fire.js
@@ -1,6 +1,9 @@
 import firebase from 'firebase';
 
   var config = {
+    apiKey: "AIzaSyCvkzFOlwPCeR2Y6dlJJC0I7bNIthvlZ5w",
+    projectId: "bollywoodmix-61cb0",
+    authDomain: "bollywoodmix-61cb0.firebaseapp.com",
     databaseURL: "https://bollywoodmix-61cb0.firebaseio.com",
     messagingSenderId: "426532497175"
   };
@@ -30,6 +33,16 @@ import firebase from 'firebase';
         console.log('Unable to get permission to notify.', err);
       })
   }
+
+  export const writeSongRequest = (data) => {
+  database.ref('Request/' + new Date()).set({
+    name: data.Name,
+    dedicateTo: data.DedicateTo,
+    date: data.date,
+    time: data.time,
+    song: data.Song
+  });
+}
 
   messaging.onMessage(function(payload) {
     console.log('onMessage: ', payload);
