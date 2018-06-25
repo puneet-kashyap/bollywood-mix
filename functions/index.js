6d1c164d39ae0380921f4db2aa6f1ec7f76ab138
Puneet Kashyap <pkashyaps@gmail.com>
2018-06-25 13:53:40 -0400

    cloud function added to get download url from storage and update database

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,27 @@
 const Notification = require("./Notification");
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const cors = require('cors')({origin: true});
+const cors = require('cors')({ origin: true });
 const nodemailer = require('nodemailer');
 
 admin.initializeApp(functions.config().firebase);
 
 var mailTransport = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: functions.config().email.id,
-      pass: functions.config().email.pass
-    }
-  });
+  service: 'Gmail',
+  auth: {
+    user: functions.config().email.id,
+    pass: functions.config().email.pass
+  }
+});
 
-  function sendSongRequestEmail(email, form) {
-    const mailOptions = {
-      from: `BollywoodMix <noreply@bollywoodmix.ca>`,
-      to: email
-    };
+function sendSongRequestEmail(email, form) {
+  const mailOptions = {
+    from: `BollywoodMix <noreply@bollywoodmix.ca>`,
+    to: email
+  };
 
-    mailOptions.subject = `Song request from BollywoodMix fan`;
-    mailOptions.text = `
+  mailOptions.subject = `Song request from BollywoodMix fan`;
+  mailOptions.text = `
     Hey Andy!
 
     A fantastic listener has requested to play a song on your show.
@@ -37,23 +37,42 @@ var mailTransport = nodemailer.createTransport({
 
     Enjoy,
     BollywoodMix Fan Club.`;
-    return mailTransport.sendMail(mailOptions).then(() => {
-      console.log('New booking email sent to:', email);
-    });
-  }
+  return mailTransport.sendMail(mailOptions).then(() => {
+    console.log('New booking email sent to:', email);
+  });
+}
 
 exports.sendBookingInfo = functions.database.ref('/Request/{name}').onCreate(event => {
-    console.log('New Entry added');
-    const original = event.data.val();
-    console.log('Values', original);
-    sendSongRequestEmail(functions.config().email.recipient,original);
+  console.log('New Entry added');
+  const original = event.data.val();
+  console.log('Values', original);
+  sendSongRequestEmail(functions.config().email.recipient, original);
 })
 
 exports.sendNotification = functions.https.onRequest((req, res) => {
-  return admin.database().ref('/Devices/').once('value').then(function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
+  return admin.database().ref('/Devices/').once('value').then(function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
       Notification(functions.config().bollywoodmix.key, childSnapshot.val().token);
     })
   });
   res.status(200).send('Notification triggered!');
+});
+
+exports.getLatestAudio = functions.storage.object().onFinalize((object) => {
+  console.log('New Audio added');
+  const audio_url = 'https://firebasestorage.googleapis.com/v0/b/'
+    + (object.bucket) + '/o/'
+    + encodeURIComponent(object.name)
+    + '?alt=media&token='
+    + object.metadata.firebaseStorageDownloadTokens;
+
+  console.log('URL', audio_url);
+  console.log(object);
+  allowedContent = ['audio/mpeg', 'audio/mp3']
+  if (allowedContent.includes(object.contentType)) {
+    admin.database().ref('/Audios/').set({
+      latestAudio: audio_url
+    });
+    console.log('Database updated with latest url');
+  }
 });
